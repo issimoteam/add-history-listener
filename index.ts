2717cdd09071d57ff394af7db36194fe14ed5053
sebastian-the-crab <seb@issimo.io>
2020-11-22 12:29:36 +1100

    add popstate listener

--- index.ts
+++ index.ts
@@ -1,20 +1,10 @@
-export { HistoryItem, HistoryCallback, HistoryCleanup };
+export { HistoryCallback, HistoryCleanup };
 export default addHistoryListener;
-
-/**
- * Represents the state of the browser history.
- */
-type HistoryItem<STATE = any> = {
-  readonly state: STATE;
-  readonly hash: string;
-  readonly pathname: string;
-  readonly search: string;
-};
 
 /**
  * A function to be called after history has changed.
  */
-type HistoryCallback<STATE = any> = (prev: HistoryItem<STATE>, next: HistoryItem<STATE>) => void;
+type HistoryCallback = () => void;
 
 /**
  * A function to clean up a subscription to the browser history.
@@ -22,42 +12,36 @@ type HistoryCallback<STATE = any> = (prev: HistoryItem<STATE>, next: HistoryItem
 type HistoryCleanup = () => void;
 
 const LISTENERS = new Set<HistoryCallback>();
-const HISTORY_METHODS = ["go", "back", "forward", "pushState", "replaceState"] as const;
+const HISTORY_METHODS = ["go", "forward", "pushState", "replaceState"] as const;
 for (const fnName of HISTORY_METHODS) {
   const symbol = Symbol(fnName);
   // @ts-expect-error
   history[symbol] = history[fnName];
   history[fnName] = (...args: unknown[]) => {
-    const prevHistoryItem = makeHistoryItem();
     // @ts-expect-error
     history[symbol](...args);
-    handleHistoryChange(prevHistoryItem);
+    handleHistoryChange();
   };
 }
 
+window.addEventListener("popstate", () => {
+  handleHistoryChange();
+});
+
 /**
  * Subscribe to browser history changes
- * @param callback will be called when "history.go", "history.back", "history.forward",
- *                 "history.pushState" or "history.replaceState" are called
+ * @param callback will be called when "history.go", "history.forward",
+ *                 "history.pushState" or "history.replaceState" are called,
+ *                 or when the "popstate" event is emitted
  * @returns a cleanup callback to unsubscribe
  */
-function addHistoryListener<STATE = any>(callback: HistoryCallback<STATE>): HistoryCleanup {
+function addHistoryListener(callback: HistoryCallback): HistoryCleanup {
   LISTENERS.add(callback);
   return () => {
     LISTENERS.delete(callback);
   };
 }
 
-function handleHistoryChange(prev: HistoryItem) {
-  const currentHistoy = makeHistoryItem();
-  for (const cb of LISTENERS) cb(prev, currentHistoy);
-}
-
-function makeHistoryItem(): HistoryItem {
-  return Object.freeze({
-    state: history.state,
-    hash: location.hash,
-    pathname: location.pathname,
-    search: location.search,
-  });
+function handleHistoryChange() {
+  for (const cb of LISTENERS) cb();
 }
